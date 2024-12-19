7593334df856448f51952abddf685972b7652fce
Yahagi123 <164115863+Yahagi123@users.noreply.github.com>
2024-12-19 10:46:07 +0800

    Update connect.php

--- Aclc Project/connect.php
+++ Aclc Project/connect.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "school_database";
+    $dbname = "aclc_data";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
     if ($conn->connect_error) {
